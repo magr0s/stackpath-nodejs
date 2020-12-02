86141c0fed076793ffdbc3b18ebb34c5cea9f71a
Oleksandr Krokhmalnyi <magr0s.ak@gmail.com>
2020-12-02 20:55:34 +0200

    chore: Add to export

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-const StacksStackpach = require('./libs/stacks')
-const SitesStackpath = require('./libs/sites')
-const CDNStackpath = require('./libs/cdn')
+const StacksStackpach = require('./libs/stacks');
+const SitesStackpath = require('./libs/sites');
+const CDNStackpath = require('./libs/cdn');
+const DeliveryDomainsStackpath = require('./libs/deliveryDomains');
 
 module.exports = {
   StacksStackpach,
   SitesStackpath,
-  CDNStackpath
+  CDNStackpath,
+  DeliveryDomainsStackpath
 }
