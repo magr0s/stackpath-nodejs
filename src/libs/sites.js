35782c31348e98ce850a06fcffd79648bdc67030
Oleksandr Krokhmalnyi <magr0s.ak@gmail.com>
2021-01-15 19:49:30 +0200

    feat: Add new methods
    - list
    - delete

--- src/libs/sites.js
+++ src/libs/sites.js
@@ -1,6 +1,36 @@
 const Stackpath = require('./stackpath')
 
 class SitesStackpath extends Stackpath {
+  list (stackId, params = {}) {
+    const requestParams = [
+      'first', 'after', 'filter', 'sort_by'
+    ];
+
+    const options = {
+      method: 'GET'
+    }
+
+    let qs = '';
+
+    if (typeof (params) === 'object') {
+      const keys = Object.keys(params);
+
+      const entries = Object.entries(
+        keys.reduce((memo, key) => {
+          if (requestParams.includes(key)) {
+            memo[`page_request.${key}`] = params[key]
+          }
+
+          return memo;
+        }, {})
+      );
+
+      qs = entries.map(([k, v]) => (`${k}=${v}`)).join('&');
+    }
+
+    return this.fetch(`delivery/v1/stacks/${stackId}/sites?${qs}`, options);
+  }
+
   add (stackId, params) {
     const options = {
       method: 'POST',
@@ -9,6 +39,14 @@ class SitesStackpath extends Stackpath {
 
     return this.fetch(`delivery/v1/stacks/${stackId}/sites`, options)
   }
+
+  delete (stackId, siteId) {
+    const options = {
+      method: 'DELETE'
+    }
+
+    return this.fetch(`delivery/v1/stacks/${stackId}/sites/${siteId}`)
+  }
 }
 
-module.exports = SitesStackpath
+module.exports = SitesStackpath;
